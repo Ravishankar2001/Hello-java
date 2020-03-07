f3f89f81f1b79f6bed3c63c7e0a6833505d8f10e
Ravishankar2001 <ravishankarnavdeep003@gmail.com>
2020-03-07 13:39:18 +0530

    first Hello World cimmit rs

---
+++ src/Bytax.java
@@ -0,0 +1,7 @@
+public class Bytax {
+    public static void main(String[] args) {
+        byte a = 123;
+        System.out.println(a);
+
+    }
+}
---
+++ src/charex.java
@@ -0,0 +1,7 @@
+public class charex {
+    public static void main(String[] args) {
+        char a = 50;
+        System.out.println(a);
+
+    }
+}
